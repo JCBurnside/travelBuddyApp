4f8c9bbedde6b05fe9e6a35eb50eeb13c595729d
James Burnside <jcburnside@gmail.com>
2017-08-07 15:51:01 -0400

    homepage linked up

--- src/app/models/trip.ts
+++ src/app/models/trip.ts
@@ -5,8 +5,10 @@ export default class Trip{
         public StartLocation:String="",
         public Destinations:String[]=new Array<string>(),
         public Price:number=0.0,
-        public StartDate:Date=new Date(),
-        public EndDate:Date=new Date(),
+        public ModeOfTransit:String="air",
+        public StartDate?:Date,
+        public EndDate?:Date,
+        public Description:String="",
         public Travellers:String[]=new Array<string>(),
         public Intrests:String[]=new Array<string>()){}
 }
