1bbd454f473b58274b482acdb538e36a239f9b4f
James Burnside <jcburnside@gmail.com>
2017-08-07 13:48:08 -0400

    doing all da tings

--- src/app/models/trip.ts
+++ src/app/models/trip.ts
@@ -1,12 +1,12 @@
 export default class Trip{
     constructor(
         public Owner:String,
-        public StartDate:Date=new Date(),
-        public EndDate:Date=new Date(),
         public Name:String,
-        public StartLocation:String,
+        public StartLocation:String="",
         public Destinations:String[]=new Array<string>(),
         public Price:number=0.0,
+        public StartDate:Date=new Date(),
+        public EndDate:Date=new Date(),
         public Travellers:String[]=new Array<string>(),
         public Intrests:String[]=new Array<string>()){}
 }
