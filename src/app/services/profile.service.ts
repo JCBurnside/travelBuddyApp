5b367defd01f6c062b9e78d68b1afde75fbc8cf8
jonathaneyster <jonathaneyster@gmail.com>
2017-08-08 09:34:24 -0400

    commit

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -4,17 +4,12 @@ import Profile from "../models/profile";
 import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
 
 @Injectable()
-<<<<<<< HEAD
+
 export class ProfileService{
     items:FirebaseListObservable<Profile[]>;
     constructor(db:AngularFireDatabase){
         this.items=db.list('profiles');
-=======
-export default class ProfileService {
-    items: FirebaseListObservable<Profile[]>;
-    constructor(db: AngularFireDatabase) {
-        this.items = db.list('profiles');
->>>>>>> 899cd9d68a96202ac681a11588f88fdf5b41c91f
+
     }
     getProfileById(id: string,cb:Function): void {
         let profile;
@@ -31,3 +26,4 @@ export default class ProfileService {
     }
 
 }
+
