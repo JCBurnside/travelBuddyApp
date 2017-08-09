697b343f8a1ea495f61ed43f100f9aa696e9744e
James Burnside <jcburnside@gmail.com>
2017-08-09 09:12:51 -0400

    PROFILE EDIT STARTED+SOME PROFILE SERVICE STUFF

--- src/app/services/routes.ts
+++ src/app/services/routes.ts
@@ -29,7 +29,7 @@ export const routes = [
     component: TripViewComponent
   },
   {
-    path     : 'profile-edit',
+    path     : 'profile-edit/:id',
     component: ProfileEditComponent
   },
   {
