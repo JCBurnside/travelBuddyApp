e3403d0a2247f5124e35df0d068e08b094e05903
James Burnside <jcburnside@gmail.com>
2017-08-09 11:29:36 -0400

    NAVBAR FIX

---
+++ src/app/nav/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FirebaseService } from "../services/auth.service";
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'ct-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+    constructor(private as:FirebaseService,private router:Router){}
+    goToPE(){
+        this.router.navigate(['/profile-edit',this.as.getId()])
+    }
+    authed(){
+        return this.as.isAuthed();
+    }
+ }
