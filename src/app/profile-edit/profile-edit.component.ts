c9e11d3bea3f5e052b3b595b037e0b0d03602b2b
Mackenzie Hartman <mackenzie.e.hartman@gmail.com>
2017-08-11 10:15:27 -0400

    added profile view/edit connections

--- src/app/profile-edit/profile-edit.component.ts
+++ src/app/profile-edit/profile-edit.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
-import Profile from "../models/profile";
+import { ActivatedRoute } from '@angular/router';
+import Profile from '../models/profile';
 
-import {ProfileService} from "../services/profile.service";
-import { FirebaseService } from "../services/auth.service";
-import { TripsService } from "../services/trips.service";
+import { ProfileService } from '../services/profile.service';
+import { FirebaseService } from '../services/auth.service';
+import { TripsService } from '../services/trips.service';
 
 @Component({
   selector: 'app-profile-edit',
@@ -12,26 +12,31 @@ import { TripsService } from "../services/trips.service";
   styleUrls: ['./profile-edit.component.css']
 })
 export class ProfileEditComponent implements OnInit {
-  public profile: Profile;
+  public profileedit: Profile;
   genderSign: string;
   constructor(private route: ActivatedRoute, private PS: ProfileService, private AS: FirebaseService) { }
   img: any;
-  email:string;
-  PID: string;//profile ID
+  email: string;
+  PID: string; // profile ID
   private sub: any;
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-      this.PS.getProfileById(params['id'], (profile:Profile) => {
-        this.profile = profile;
+      this.PS.getProfileById(params['id'], (profile: Profile) => {
+        this.profileedit = profile;
+        // if (this.profileedit.Gender == 'female') {
+        //   this.genderSign = './img/female.png';
+        // }
       });
     });
 
-    this.img = this.AS.currentUser().photoURL;
-    this.email=this.AS.currentUser().email;
-    if(this.profile.Gender=="female"){
-      this.genderSign="./img/female.png";
-    }
   }
-  updateImg(img){}
+  updateImg(img) { }
+  onSubmit() {
+    this.PS.saveProfile(this.profileedit, (profile, err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 
 }
