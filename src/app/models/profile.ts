ac37d1c13d9319c96fddf0cab54b4fc954e7378b
James Burnside <jcburnside@gmail.com>
2017-08-17 11:01:54 -0400

    Fackbooks

--- src/app/models/profile.ts
+++ src/app/models/profile.ts
@@ -1,14 +1,15 @@
 import { Interests } from "./interests";
 export default class Profile{
     constructor(
-        public ownerID    : string,
-        public Name       : String='',
-        public Interest   : Interests=new Interests(),
-        public Gender     : String='female',
-        public Headline   : String='',
-        public Occupation : String='',
-        public ImgURL     : string='',
-        public Hometown   : String='',
-        public $key       : string=''
+        public ownerID   : string,
+        public Name      : String='',
+        public Interest  : Interests=new Interests(),
+        public Gender    : String='female',
+        public Headline  : String='',
+        public Occupation: String='',
+        public ImgURL    : string='',
+        public Hometown  : String='',
+        public Facebook  : string='',
+        public $key      : string=''
     ){}
 }
